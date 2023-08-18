f446f68fbd76023028058a26909ed0e78ba482b2
Viraj76 <virajg.7680@gmail.com>
2023-08-19 02:57:19 +0530

    Initial commits

---
+++ app/src/main/java/com/example/quotes/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.quotes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quotes.screens.QuoteDetail
+import com.example.quotes.screens.QuotesListItem
+import com.example.quotes.ui.theme.QuotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QuoteDetail()
+            QuotesListItem()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quotes/screens/QuotesListItem.kt
@@ -0,0 +1,128 @@
+package com.example.quotes.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FormatQuote
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+//@Preview
+@Composable
+fun QuotesListItem() {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = Modifier.padding(5.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                imageVector = Icons.Outlined.FormatQuote,
+                contentDescription = "Quote",
+                colorFilter = ColorFilter.tint(Color.White),
+                alignment = Alignment.TopStart,
+                modifier = Modifier
+                    .background(Color.Black)
+                    .rotate(180f)
+                    .size(60.dp)
+            )
+            Spacer(modifier = Modifier.padding(5.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "This a quote , that be consistent in your work don't focus on others",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(.4f)
+                        .background(Color.Gray)
+                        .height(1.dp)
+                )
+                Text(
+                    text = "Viraj Gupta",
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(top = 8.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun QuoteDetail() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff),
+                        Color(0xFFE3E3E3)
+                    )
+                )
+            )
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 7.dp),
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(10.dp, 20.dp)
+            ) {
+                Image(
+                    imageVector = Icons.Outlined.FormatQuote,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .rotate(180f)
+                        .background(Color.Black),
+                    colorFilter = ColorFilter.tint(Color.White)
+
+                )
+                Text(
+                    text = "This is very sweet quote like you!",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontSize = 23.sp
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = "Viraj Gupta",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+    }
+}
